81868977b281e2d493fbf3f68158f7a761978846
KISHOR <158146999+KISHOR1072@users.noreply.github.com>
2025-05-29 14:09:59 +0530

    Add files via upload

---
+++ db.py
@@ -0,0 +1,48 @@
+import pymongo
+import os
+from flask import request
+from dotenv import load_dotenv
+
+load_dotenv()
+
+Mongo_uri = os.getenv("MONGO_URI")
+
+client = pymongo.MongoClient(MONGO_URI)
+userdb = client['userdb']
+users = userdb.customers
+
+
+def insert_data():
+	if request.method == 'POST':
+		name = request.form['name']
+		email = request.form['email']
+		password = request.form['pass']
+
+		reg_user = {}
+		reg_user['name'] = name
+		reg_user['email'] = email
+		reg_user['password'] = password
+
+		if users.find_one({"email":email}) == None:
+			users.insert_one(reg_user)
+			return True
+		else:
+			return False
+
+
+def check_user():
+
+	if request.method == 'POST':
+		email = request.form['email']
+		password = request.form['pass']
+
+		user = {
+			"email": email,
+			"password": password
+		}
+
+		user_data = users.find_one(user)
+		if user_data == None:
+			return False, ""
+		else:
+			return True, user_data["name"]
